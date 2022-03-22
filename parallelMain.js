6677c4d8f3fe4f76e6de82f22788cce3b7bbc7a3
Depik400 <pro100cuber@gmail.com>
2022-03-22 19:46:26 +0700

    fix

--- parallelMain.js
+++ parallelMain.js
@@ -54,24 +54,38 @@ async function print(from, result) {
     console.log(`${from}\t${Math.floor(result.time*10**5)/10**5}`);
 }
 
+function printArr(arr) {
+    console.log("Для копирования - " + arr.reduce((a, b) => a + "\t" + b))
+}
+
 async function main() {
     let from = 32768;
     let to = 512 * 4096;
+    let arr = [];
     console.log(chalk.bgBlue('Обычная'));
     for (let i = from; i <= to; i *= 2) {
         let result = await run(i, "default");
+        arr.push(result.time);
         print(i, result);
     }
+    printArr(arr);
+    arr = [];
     console.log(chalk.bgBlue('Только исключения'));
     for (let i = from; i <= to; i *= 2) {
         let result = await run(i, "onlyException");
+        arr.push(result.time);
         print(i, result);
     }
+    printArr(arr);
+    arr = [];
     console.log(chalk.bgBlue('Без исключений'));
     for (let i = from; i <= to; i *= 2) {
         let result = await run(i, "withoutException");
+        arr.push(result.time);
         print(i, result);
     }
+    printArr(arr);
+    arr = [];
 }
 
 main();
